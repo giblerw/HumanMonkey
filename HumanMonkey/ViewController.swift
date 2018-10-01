1acac7f3df526ddd75ad6bf46c03f9e9804cc5bf
GiblerW <westongibler@gmail.com>
2018-10-01 01:16:02 -0600

    Login page is layedout. Signup button switches with login if new

--- HumanMonkey/ViewController.swift
+++ HumanMonkey/ViewController.swift
@@ -10,22 +10,47 @@ import UIKit
 import Parse
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        let gameScore = PFObject(className:"GameScore")
-        gameScore["score"] = 1337
-        gameScore["playerName"] = "Sean Plott"
-        gameScore["cheatMode"] = false
-        gameScore.saveInBackground { (success, error) in
-            if (success) {
-                print("woohoo")
-            } else {
-                print("oops")
-            }
+    
+    var signupModeActive = true
+    
+//Login text fields
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+//Signup button
+    @IBAction func signupOrLogin(_ sender: Any) {
+        if email.text == "" || password.text == "" {
+            let alert = UIAlertController(title: "Error in form input", message: "Please enter a valid email & password", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action) in
+                self.dismiss(animated: true, completion: nil)
+            }))
+            self.present(alert, animated: true, completion: nil)
+        } else {
+            print("Signing up...")
         }
     }
+    @IBOutlet weak var signupOrLoginButton: UIButton!
+    
+//Login button
+    @IBAction func switchLoginMode(_ sender: Any) {
+        //First, findout which signup mode we are in
+        if (signupModeActive) {
+            //Change things from signupMode to loginMode
+            signupModeActive = false
+            signupOrLoginButton.setTitle("Log In", for: [])
+            switchLoginModeButton.setTitle("Sign Up", for: [])
+        } else {
+            //Change things from loginMode to  signupMode
+            signupModeActive = true
+            signupOrLoginButton.setTitle("Sign Up", for: [])
+            switchLoginModeButton.setTitle("Log in", for: [])
+        }
+    }
+    @IBOutlet weak var switchLoginModeButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
+    }
 }
 
